0200c31c1a039152a1090dfeebb95d88347b1da9
miaoever <leo.miao.ever@gmail.com>
2014-11-11 17:55:00 +0800

    router support adding listeners and broadcasting.

--- verify/wsServer_spec.js
+++ verify/wsServer_spec.js
@@ -1,9 +1,9 @@
-var express = require("../")
-  , request = require("supertest")
+var request = require("supertest")
   , expect = require("chai").expect
   , http = require("http")
   , wsClient = require("websocket").client
-  , wsServer = require("../lib/server");
+  , wsServer = require("../lib/server")
+  , monkey_patch_wsConnection = require("../lib/monkey_patch_wsConnection");
 
 describe("websocket server should listens on specific port", function() {
   var client;
@@ -12,21 +12,28 @@ describe("websocket server should listens on specific port", function() {
   router = wsServer.createServer(8080);
 
   router.mount("/echo", "etherpty-protocol", function(request) {
-    var connection = require("../lib/monkey_patch_wsConnection")(request.accept('etherpty-protocol', request.origin));
+    var connection = monkey_patch_wsConnection(request.accept('etherpty-protocol', request.origin));
     connection.on("message", function(message) {
       connection.send("pong");
     });
   })
 
   router.mount("/pty/io/:token", "etherpty-protocol", function(request) {
-    var connection = require("../lib/monkey_patch_wsConnection")(request.accept('etherpty-protocol', request.origin));
+    var connection = monkey_patch_wsConnection(request.accept('etherpty-protocol', request.origin));
     connection.send(request.params.token);
   });
 
   router.mount("/pty/meta/:token", "etherpty-protocol", function(request) {
-    var connection = require("../lib/monkey_patch_wsConnection")(request.accept('etherpty-protocol', request.origin));
+    var connection = monkey_patch_wsConnection(request.accept('etherpty-protocol', request.origin));
+    connection.on("share", function(data) {
+      connection.send(data.token);
+    });
+
     connection.on("join", function(data) {
-      connection.send(data);
+      router.addListener(data.token,"meta", connection);
+      if (router.listeners[data.token]["meta"].length === 2) {
+        router.broadcast(data.token, "meta", "broadcast message.") ;
+      }
     });
   });
 
@@ -45,7 +52,7 @@ describe("websocket server should listens on specific port", function() {
     client.connect('ws://localhost:8080/echo', 'etherpty-protocol');
   });
 
-  it("responds /pty/io/:token with token.", function(done) {
+  it("support express-style path, eg: /pty/io/:token ", function(done) {
     client.on("connect", function(connection) {
       connection.on("message", function(message) {
         expect(message.utf8Data).to.equal("mytoken");
@@ -58,7 +65,7 @@ describe("websocket server should listens on specific port", function() {
 
   it("monkey patch for the connection to listening on specific event.", function(done) {
     client.on("connect", function(connection) {
-      connection.send(JSON.stringify({type:"join", token:"mytoken"}))
+      connection.send(JSON.stringify({type:"share", token:"mytoken"}))
       connection.on("message", function(message) {
         expect(message.utf8Data).to.equal("mytoken");
         done();
@@ -66,6 +73,36 @@ describe("websocket server should listens on specific port", function() {
 
     });
     client.connect('ws://localhost:8080/pty/io/mytoken', 'etherpty-protocol');
+  });
+
+  it("listeners could be added to the router, and router should broadcast messages to them.", function(done) {
+    var client2 = new wsClient();
+    var count = 0;
+
+    client.on("connect", function(connection) {
+      connection.send(JSON.stringify({type:"join", token:"room1"}));
+      connection.on("message", function(message) {
+        expect(message.utf8Data).to.equal("broadcast message.");
+        count ++;
+        if (count == 2) {
+          done();
+        }
+      });
+    });
+
+    client2.on("connect", function(connection) {
+      connection.send(JSON.stringify({type:"join", token:"room1"}));
+      connection.on("message", function(message) {
+        expect(message.utf8Data).to.equal("broadcast message.");
+        count ++;
+        if (count == 2) {
+          done();
+        }
+      });
+    });
+    client.connect('ws://localhost:8080/pty/meta/mytoken', 'etherpty-protocol');
+    client2.connect('ws://localhost:8080/pty/meta/mytoken', 'etherpty-protocol');
+
   });
 
 })
