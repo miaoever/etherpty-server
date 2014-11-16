301c4f56127f787c01fb4c769c65aa3b44ee40fe
miaoever <leo.miao.ever@gmail.com>
2014-11-16 19:30:09 +0800

    support command line argvs.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-exports.router = require("./lib/server.js");
-exports.etherpty = require("./lib/etherpty.js").etherpty;
+var wsRouter = require("./lib/server.js");
+var etherpty = require("./lib/etherpty.js").etherpty;
+
+exports.etherpty = etherpty;
+exports.router = wsRouter;
+
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv.p || "8080";
+var address = argv.a || "0.0.0.0";
+var router = etherpty(wsRouter.createServer(port, address));
+
+
+
+
