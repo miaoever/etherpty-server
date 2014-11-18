eda95cbf73d5a36cf1264a5d25826ebfd943f3a2
miaoever <leo.miao.ever@gmail.com>
2014-11-18 21:11:59 +0800

    add express server.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var wsRouter = require("./lib/server.js");
 var etherpty = require("./lib/etherpty.js").etherpty;
+var express = require('express');
+var app = express();
 
 exports.etherpty = etherpty;
 exports.router = wsRouter;
@@ -8,6 +10,9 @@ var argv = require('minimist')(process.argv.slice(2));
 var port = argv.p || "8080";
 var address = argv.a || "0.0.0.0";
 var router = etherpty(wsRouter.createServer(port, address));
+
+app.use(express.static(__dirname + '/static'));
+app.listen(80);
 
 
 
