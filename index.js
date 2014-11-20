e0d2a7ac0958c73a27912ca0dbeb0f79d722265a
miaoever <leo.miao.ever@gmail.com>
2014-11-20 17:05:19 +0800

    add config.js into web client.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var wsRouter = require("./lib/server.js");
 var etherpty = require("./lib/etherpty.js").etherpty;
+var config = require("./config/config.json");
 
 exports.etherpty = etherpty;
 exports.router = wsRouter;
 
 var argv = require('minimist')(process.argv.slice(2));
-var port = argv.p || "8080";
+
+var port = argv.p || config.server.port;
 var address = argv.a || "0.0.0.0";
 var router = etherpty(wsRouter.createServer(port, address));
 
--- web/web.js
+++ web/web.js
@@ -1,15 +1,18 @@
 var express = require('express');
+var config = require('../config/config.json');
+
 var app = express();
 app.set ('views', __dirname + '/views');
 app.set ('view engine', 'ejs');
 app.use('/js', express.static(__dirname + '/public'));
-  //app.use(express.cookieParser('etherpty-secret-81119891'));
-  //app.use(express.session());
-var cookieParser = require('cookie-parser')
-app.use(cookieParser())
 
 app.get('/:token', function(req, res){
-  res.render('index.ejs',{"token":req.params.token})
+  var proto = config.server.protocol;
+  var host = config.server.host;
+  var port = config.server.port;
+  var url = proto + "://" + host + ":" + port;
+
+  res.render('index.ejs',{"token":req.params.token, "url":url});
 });
 
-app.listen(8082);
+app.listen(config.web.port);
